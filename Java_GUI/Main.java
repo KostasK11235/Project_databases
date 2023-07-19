1c96b0bc17b998771778501085f951d5ded1465f
kostas <kapolasnikos@gmail.com>
2023-07-19 20:28:19 +0300

    started with the GUI

---
+++ Java_GUI/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Main
+{
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginScreen().setVisible(true);
+            }
+        });
+    }
+}
